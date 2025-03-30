41936d7d0647045674228057d6337c1bc5f6fe1e
hotsawz-mia <limiter_inks.0k@icloud.com>
2025-03-30 08:28:46 +0100

    Initial Commit

---
+++ HFTH/HFTHApp.swift
@@ -0,0 +1,17 @@
+//
+//  HFTHApp.swift
+//  HFTH
+//
+//  Created by Mark James on 30/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct HFTHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
